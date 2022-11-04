39a2b53575ee441b4030328cf6bdadd69a55ae86
Andy Freidenfelds <afreidenfelds@gmail.com>
2022-11-04 10:42:09 -0500

    adds: env config for azure

--- src/auth.ts
+++ src/auth.ts
@@ -4,13 +4,10 @@ import { BearerStrategy, ITokenPayload } from "passport-azure-ad";
 const scope = ["access_as_user"];
 export const strategy = new BearerStrategy(
   {
-    identityMetadata:
-      "https://login.microsoftonline.com/e85feadf-11e7-47bb-a160-43b98dcc96f1/v2.0/.well-known/openid-configuration",
-    issuer:
-      "https://login.microsoftonline.com/e85feadf-11e7-47bb-a160-43b98dcc96f1/v2.0",
-    clientID: "e3e6d70e-ff2b-4c9a-b773-1bd7d9919593",
-    audience: "e3e6d70e-ff2b-4c9a-b773-1bd7d9919593",
-    // loggingLevel: "info",
+    identityMetadata: process.env.AZURE_ID_META_URL || "",
+    issuer: process.env.AZURE_ISSUER_URL || "",
+    clientID: process.env.AZURE_CLIENT_ID || "",
+    audience: process.env.AZURE_CLIENT_ID || "",
     validateIssuer: true,
     scope,
   },
--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const dbclient = new MongoClient(
-  "mongodb://timely-test:K2sD629SlMx8IzOgzg67KkWhmw1EmySkWUs5q14GHcnPHz6mutHWa57bvjYrqaCjOebtOjvIFpKWACDbKtdKiQ==@timely-test.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@timely-test@"
-);
+const dbclient = new MongoClient(process.env.AZURE_COSMOS_URL || "");
 
 export function connect() {
   return dbclient.connect();
