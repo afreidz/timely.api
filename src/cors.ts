642997777c8eda6be1166b4f8a1089003234a32b
Andy Freidenfelds <afreidenfelds@gmail.com>
2022-11-09 12:56:00 -0600

    updates: cors logic to startswith

--- src/cors.ts
+++ src/cors.ts
@@ -4,8 +4,9 @@ function checkOrigin(
   origin: string | undefined,
   cb: (err: Error | null, o?: string) => void
 ) {
+  const origins = process.env.AC_CORS_ORIGIN?.split(",");
   if (origin?.startsWith("http://localhost:")) return cb(null, origin);
-  if (origin && process.env.AC_CORS_ORIGIN?.split(",").includes(origin)) {
+  if (origin && origins?.some((o) => origin.startsWith(o))) {
     return cb(null, origin);
   }
   return cb(new Error("Origin not allowed"));
